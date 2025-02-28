5c8ba4afd806455fcdf1809dbded8821bd578704
Alexandru Marcu <alexandru.marcu@ing.com>
2025-02-28 16:10:57 +0200

    added variable instead of image name and version

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
 def call() {
-    echo 'building the application... This is a Jenkins Shared Library function call'
+    echo "building the application... This is a Jenkins Shared Library function call $BRANCH_NAME"
     sh 'mvn package'
 }
